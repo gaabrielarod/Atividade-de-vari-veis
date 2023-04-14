a58467af57bf373322f10181ae3973b53b29430d
gaabrielarod <gabrielasouzarod@gmail.com>
2023-04-14 18:57:57 -0300

    atividade de laços de decisão

---
+++ src/lacoDecisao2/exer5switch.java
@@ -0,0 +1,66 @@
+package lacoDecisao2;
+
+import java.util.Scanner;
+
+public class exer5switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner ler = new Scanner(System.in);
+		
+		float valorTotal;
+		int quantidade,codigo;
+		
+		System.out.println("Nosso cardápio");
+		System.out.println("1 - Cachorro quente R$ 10,00");
+		System.out.println("2 - X-salada R$15,00");
+		System.out.println("3 - X-bacon R$18,00");
+		System.out.println("4 - Bauru R$12,00");
+		System.out.println("5 - Refrigerante R$8,00");
+		System.out.println("6 - Suco de laranja R$13,00");
+		
+		System.out.println("Digite o número correspondente ao produto que você quer: ");
+		codigo = ler.nextInt();
+		
+		System.out.println("Digite a quantidade do produto: ");
+		quantidade = ler.nextInt();
+		
+		switch(codigo) {
+		case 1:
+			valorTotal = quantidade * 10;
+			System.out.println("O valor total deu "+valorTotal);
+			System.out.println("Qual será a forma de pagamento?");
+			break;
+		case 2:
+			valorTotal = quantidade * 15;
+			System.out.println("O valor total deu "+valorTotal);
+			System.out.println("Qual será a forma de pagamento?");
+			break;
+		case 3:
+			valorTotal = quantidade * 18;
+			System.out.println("O valor total deu "+valorTotal);
+			System.out.println("Qual será a forma de pagamento?");
+			break;
+		case 4:
+			valorTotal = quantidade * 12;
+			System.out.println("O valor total deu "+valorTotal);
+			System.out.println("Qual será a forma de pagamento?");
+			break;
+		case 5:
+			valorTotal = quantidade * 8;
+			System.out.println("O valor total deu "+valorTotal);
+			System.out.println("Qual será a forma de pagamento?");
+			break;
+		case 6:
+			valorTotal = quantidade * 13;
+			System.out.println("O valor total deu "+valorTotal);
+			System.out.println("Qual será a forma de pagamento?");
+			break;
+			
+		default:
+				System.out.println("Opção inválida");
+		}
+	}
+
+}
---
+++ src/lacoDecisao2/exer6switch.java
@@ -0,0 +1,74 @@
+package lacoDecisao2;
+
+import java.util.Scanner;
+
+public class exer6switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+		
+		String nome;
+		int codigo;
+		float salario;
+		double salarioNovo, reajusteSalario;
+		
+		System.out.println("Segue a tabela com o código para cada cargo:");
+		System.out.println("1 - Gerente");
+		System.out.println("2 - Vendedor");
+		System.out.println("3 - Supervisor");
+		System.out.println("4 - Motorista");
+		System.out.println("5 - Estoquista");
+		System.out.println("6 - Técnico de TI");
+		
+		System.out.println("Digite o seu nome: ");
+		nome = leia.next();
+		
+		System.out.println("Digite o código respectivo ao seu cargo: ");
+		codigo = leia.nextInt();
+		
+		System.out.println("Digite o seu salário: ");
+		salario = leia.nextFloat();
+		
+		switch(codigo) {
+		case 1:
+			reajusteSalario = 0.10;
+			salarioNovo = salario + (salario * reajusteSalario ) ;
+			System.out.println(nome+" seu cargo é Gerente e o reajuste do seu salário ficou: "+salarioNovo);
+			break;
+		case 2:
+			reajusteSalario = 0.07;
+			salarioNovo = salario + (salario * reajusteSalario ) ;
+			System.out.println(nome+" seu cargo é Vendedor e o reajuste do seu salário ficou: "+salarioNovo);
+			break;
+		case 3:
+			reajusteSalario = 0.09;
+			salarioNovo = salario + (salario * reajusteSalario ) ;
+			System.out.println(nome+" seu cargo é Supervisor e o reajuste do seu salário ficou: "+salarioNovo);
+			break;
+		case 4:
+			reajusteSalario = 0.06;
+			salarioNovo = salario + (salario * reajusteSalario ) ;
+			System.out.println(nome+" seu cargo é Motorista e o reajuste do seu salário ficou: "+salarioNovo);
+			break;
+		case 5:
+			reajusteSalario = 0.05;
+			salarioNovo = salario + (salario * reajusteSalario ) ;
+			System.out.println(nome+" seu cargo é Estoquista e o reajuste do seu salário ficou: "+salarioNovo);
+			break;
+		case 6:
+			reajusteSalario = 0.08;
+			salarioNovo = salario + (salario * reajusteSalario ) ;
+			System.out.println(nome+" seu cargo é Técnico de TI e o reajuste do seu salário ficou: "+salarioNovo);
+			break;
+			
+			default:
+				System.out.println("Erro");
+
+				
+		}
+		
+	}
+
+}
